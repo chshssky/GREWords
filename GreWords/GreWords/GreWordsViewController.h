de471d12b1ee5214542d46b392a307b7d0e9e670
chshssky <chshssky@gmail.com>
2013-03-26 23:13:24 +0800

    again

---
+++ GreWords/GreWords/GreWordsViewController.h
@@ -0,0 +1,13 @@
+//
+//  GreWordsViewController.h
+//  GreWords
+//
+//  Created by 崔 昊 on 13-3-26.
+//  Copyright (c) 2013年 Cui Hao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GreWordsViewController : UIViewController
+
+@end
---
+++ GreWords/GreWords/GreWordsViewController.m
@@ -0,0 +1,29 @@
+//
+//  GreWordsViewController.m
+//  GreWords
+//
+//  Created by 崔 昊 on 13-3-26.
+//  Copyright (c) 2013年 Cui Hao. All rights reserved.
+//
+
+#import "GreWordsViewController.h"
+
+@interface GreWordsViewController ()
+
+@end
+
+@implementation GreWordsViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
