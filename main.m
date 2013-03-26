4d6b72fb94a66161b8380bfaf047fbb14efa10aa
chshssky <chshssky@gmail.com>
2013-03-26 20:35:32 +0800

    Initial Commit

---
+++ main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  GreWords
+//
+//  Created by 崔 昊 on 13-3-26.
+//  Copyright (c) 2013年 Cui Hao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "GreWordsAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([GreWordsAppDelegate class]));
+    }
+}
