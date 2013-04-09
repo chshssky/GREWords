b9d0e2d5f37fea65dd4988fa46ab4c3bd3ba1ff6
chshssky <chshssky@gmail.com>
2013-04-09 16:49:35 +0800

    Add Some NSManagedObjects

---
+++ GreWords/GreWords/Note.m
@@ -0,0 +1,18 @@
+//
+//  Note.m
+//  GreWords
+//
+//  Created by 崔 昊 on 13-4-9.
+//  Copyright (c) 2013年 Cui Hao. All rights reserved.
+//
+
+#import "Note.h"
+#import "Word.h"
+
+
+@implementation Note
+
+@dynamic content;
+@dynamic note2word;
+
+@end
---
+++ GreWords/GreWords/Word.h
@@ -0,0 +1,21 @@
+//
+//  Word.h
+//  GreWords
+//
+//  Created by 崔 昊 on 13-4-9.
+//  Copyright (c) 2013年 Cui Hao. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+@class Note;
+
+@interface Word : NSManagedObject
+
+@property (nonatomic, retain) NSString * lastChecks;
+@property (nonatomic, retain) NSDate * lastMistakeTime;
+@property (nonatomic, retain) NSNumber * plistID;
+@property (nonatomic, retain) Note *word2note;
+
+@end
