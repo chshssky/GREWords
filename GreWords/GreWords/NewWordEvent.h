bd8715505030ac85aa229bad17e156f2f6027e19
chshssky <chshssky@gmail.com>
2013-04-09 18:40:43 +0800

    写好了四个Event 并对项目文件的结构进行了组织 还添了Stage枚举类型

---
+++ GreWords/GreWords/NewWordEvent.h
@@ -0,0 +1,21 @@
+//
+//  NewWordEvent.h
+//  GreWords
+//
+//  Created by 崔 昊 on 13-4-9.
+//  Copyright (c) 2013年 Cui Hao. All rights reserved.
+//
+
+#import "BaseEvent.h"
+
+@interface NewWordEvent : BaseEvent
+
+@property (nonatomic) enum Stage stage_now;
+@property (nonatomic) int unit;
+@property (nonatomic) int round;
+@property (nonatomic) int orderInUnit;
+@property (nonatomic) NSTimeInterval duration;
+@property (nonatomic) int totalWordCount;
+@property (nonatomic) int wrongWordCount;
+
+@end
