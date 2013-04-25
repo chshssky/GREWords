cae1341149bc1b3019cbcee6b084b9d1e003ac42
chshssky <chshssky@gmail.com>
2013-04-25 21:09:10 +0800

    初步加入了WordDetail界面

---
+++ GreWords/GreWords/UILabel+Transparent.h
@@ -0,0 +1,13 @@
+//
+//  UILabel+Transparent.h
+//  GreWords
+//
+//  Created by 崔 昊 on 13-4-25.
+//  Copyright (c) 2013年 Cui Hao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UILabel (Transparent)
+
+@end
