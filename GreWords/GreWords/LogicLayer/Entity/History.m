e49a4eae05e9e693a88af9944156f49c9472d9f0
Song <sbhhbscbfv@gmail.com>
2013-05-09 21:55:47 +0800

    WTF

---
+++ GreWords/GreWords/LogicLayer/Entity/History.m
@@ -0,0 +1,22 @@
+//
+//  History.m
+//  GreWords
+//
+//  Created by 崔 昊 on 13-4-10.
+//  Copyright (c) 2013年 Cui Hao. All rights reserved.
+//
+
+#import "History.h"
+
+
+@implementation History
+
+@dynamic endTime;
+@dynamic event;
+@dynamic info;
+@dynamic startTime;
+@dynamic totalWordCount;
+@dynamic wrongWordCount;
+@dynamic duration;
+
+@end
