c1049810c2d90c368941c5a1f88f8863d8da8da2
Song <sbhhbscbfv@gmail.com>
2013-05-30 21:27:29 +0800

    oh yes smart list search!

--- GreWords/GreWords/Libs/CustomSearchBar.m
+++ GreWords/GreWords/Libs/CustomSearchBar.m
@@ -7,7 +7,7 @@
     self = [super initWithCoder:aDecoder];
     if (self) {
         customBackButton = [UIButton buttonWithType:UIButtonTypeCustom];
-        customBackButton.frame = CGRectMake(283, 13, 20, 21);
+        customBackButton.frame = CGRectMake(283, 11, 20, 21);
         
         [customBackButton addTarget:self action:@selector(cancelAction) forControlEvents:UIControlEventTouchUpInside];
         [customBackButton setBackgroundImage:[UIImage imageNamed:@"findIcon.png"] forState:UIControlStateNormal];
@@ -20,25 +20,27 @@
 
 -(void)drawRect:(CGRect)rect {
     [[[self subviews] objectAtIndex:0] setAlpha:0.0];
-    UIImage *image = [UIImage imageNamed: @"UISearchBarBG.png"];    
+    UIImage *image = [UIImage imageNamed: @"UISearchBarBG.png"];
     [image drawInRect:CGRectMake(0, 0, self.frame.size.width, self.frame.size.height)];
 }
 
 -(void)layoutSubviews {
+    [super layoutSubviews];
     [self setShowsCancelButton:NO animated:NO];
     
     UITextField *textfield = [self textView];
-    
+    [textfield setBorderStyle:UITextBorderStyleNone];
     UIView *leftview = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 5, 20)];
         
     textfield.leftView = leftview;
     textfield.clearButtonMode = UITextFieldViewModeNever;
     textfield.backgroundColor = [UIColor clearColor];
-    [textfield setBorderStyle:UITextBorderStyleNone];
-    textfield.layer.borderColor = [[UIColor clearColor] CGColor];
+    [textfield setBackground:[UIImage imageNamed: @"UISearchBarText.png"]];  
+    
+    //textfield.layer.borderColor = [[UIColor clearColor] CGColor];
     textfield.textColor = [UIColor colorWithRed:224/255.0 green:210/255.0 blue:193/255.0 alpha:1.0];
     
-    [super layoutSubviews];
+    
 }
 
 -(UIButton*)searchButton
