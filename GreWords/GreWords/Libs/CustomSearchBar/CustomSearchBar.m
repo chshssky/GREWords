943809b916ee022527b22166fa8594bf7f4551b3
Song <sbhhbscbfv@gmail.com>
2013-06-02 20:13:34 +0800

    add core plot and reorganize libs

---
+++ GreWords/GreWords/Libs/CustomSearchBar/CustomSearchBar.m
@@ -0,0 +1,81 @@
+#import "CustomSearchBar.h"
+#import <QuartzCore/QuartzCore.h>
+#import "UIColor+RGB.h"
+@implementation CustomSearchBar
+
+-(id)initWithCoder:(NSCoder *)aDecoder {
+    self = [super initWithCoder:aDecoder];
+    if (self) {
+        customBackButton = [UIButton buttonWithType:UIButtonTypeCustom];
+        customBackButton.frame = CGRectMake(283, 11, 20, 21);
+        
+        [customBackButton addTarget:self action:@selector(cancelAction) forControlEvents:UIControlEventTouchUpInside];
+        [customBackButton setBackgroundImage:[UIImage imageNamed:@"findIcon.png"] forState:UIControlStateNormal];
+        
+        [self addSubview:customBackButton];
+        //Set other button states (hightlight, select, etc) here
+    }    
+    return self;
+} 
+
+-(void)drawRect:(CGRect)rect {
+    [[[self subviews] objectAtIndex:0] setAlpha:0.0];
+    UIImage *image = [UIImage imageNamed: @"UISearchBarBG.png"];
+    [image drawInRect:CGRectMake(0, 0, self.frame.size.width, self.frame.size.height)];
+}
+
+-(void)layoutSubviews {
+    [super layoutSubviews];
+    [self setShowsCancelButton:NO animated:NO];
+    
+    UITextField *textfield = [self textView];
+    [textfield setBorderStyle:UITextBorderStyleNone];
+    UIView *leftview = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 5, 20)];
+        
+    textfield.leftView = leftview;
+    textfield.clearButtonMode = UITextFieldViewModeNever;
+    textfield.backgroundColor = [UIColor clearColor];
+    [textfield setBackground:[UIImage imageNamed: @"UISearchBarText.png"]];  
+    
+    //textfield.layer.borderColor = [[UIColor clearColor] CGColor];
+    textfield.textColor = [UIColor colorWithR:135 G:122 B:122];
+    
+}
+
+-(UIButton*)searchButton
+{
+    for(id cc in [self subviews])
+    {
+        if([cc isKindOfClass:[UIButton class]])
+        {
+            if(cc == customBackButton)
+                continue;
+            UIButton *btn = (UIButton *)cc;
+            return btn;
+        }
+    }
+    return nil;
+}
+
+-(UITextField*)textView
+{
+    for(id cc in [self subviews])
+    {
+        
+        if([cc isKindOfClass:[UITextField class]])
+        {
+            
+            UITextField *btn = (UITextField *)cc;
+            return btn;
+        }
+    }
+    return nil;
+}
+
+-(void)cancelAction {
+    UIButton *btn = [self searchButton];
+    [btn sendActionsForControlEvents: UIControlEventTouchUpInside];
+    [[self textView] resignFirstResponder];
+}
+
+@end
---
+++ GreWords/GreWords/Libs/TabBarController/MHTabBarController.m
@@ -0,0 +1,241 @@
+/*!
+ * \file MHTabBarController.m
+ *
+ * Copyright (c) 2011 Matthijs Hollemans
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+#import "MHTabBarController.h"
+
+static const float TAB_BAR_HEIGHT = 40.0f;
+static const NSInteger TAG_OFFSET = 1000;
+
+@implementation MHTabBarController
+{
+	UIView *tabButtonsContainerView;
+	UIImageView *indicatorImageView;
+}
+
+- (void)centerIndicatorOnButton:(UIButton *)button
+{
+	CGRect rect = indicatorImageView.frame;
+	rect.origin.x = button.center.x - floorf(indicatorImageView.frame.size.width/2.0f);
+	rect.origin.y = TAB_BAR_HEIGHT - indicatorImageView.frame.size.height;
+	indicatorImageView.frame = rect;
+	indicatorImageView.hidden = NO;
+}
+
+- (void)selectTabButton:(UIButton *)button
+{
+	[button setTitleColor:[UIColor redColor] forState:UIControlStateNormal];
+
+	UIImage *image = [[UIImage imageNamed:@"words list_yellow.png"] stretchableImageWithLeftCapWidth:0 topCapHeight:0];
+	[button setBackgroundImage:image forState:UIControlStateNormal];
+	[button setBackgroundImage:image forState:UIControlStateHighlighted];
+    
+    [button setTitleColor:[UIColor colorWithRed:229/255.0f green:219/255.0f blue:205/255.0f alpha:1.0f] forState:UIControlStateNormal];
+	
+}
+
+- (void)deselectTabButton:(UIButton *)button
+{
+	[button setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+
+	UIImage *image = [[UIImage imageNamed:@""] stretchableImageWithLeftCapWidth:1 topCapHeight:0];
+	[button setBackgroundImage:image forState:UIControlStateNormal];
+	[button setBackgroundImage:image forState:UIControlStateHighlighted];
+    
+    [button setTitleColor:[UIColor colorWithRed:229/255.0f green:219/255.0f blue:205/255.0f alpha:1.0f] forState:UIControlStateNormal];
+
+}
+
+- (void)removeTabButtons
+{
+	NSArray *buttons = [tabButtonsContainerView subviews];
+	for (UIButton *button in buttons)
+		[button removeFromSuperview];
+}
+
+- (void)addTabButtons
+{
+	NSUInteger index = 0;
+	for (NSString *title in self.viewControllerTitles)
+	{
+		UIButton *button = [UIButton buttonWithType:UIButtonTypeCustom];
+		button.tag = TAG_OFFSET + index;
+		[button setTitle:title forState:UIControlStateNormal];
+		[button addTarget:self action:@selector(tabButtonPressed:) forControlEvents:UIControlEventTouchDown];
+		button.titleLabel.font = [UIFont boldSystemFontOfSize:14];
+		
+        [button setTitleShadowColor:[UIColor clearColor] forState:UIControlStateNormal];
+
+		[self deselectTabButton:button];
+        if(index == 0)
+            [self selectTabButton:button];
+		[tabButtonsContainerView addSubview:button];
+
+		++index;
+	}
+    NSLog(@"%@",tabButtonsContainerView.subviews);
+}
+
+- (void)reloadTabButtons
+{
+	[self removeTabButtons];
+	[self addTabButtons];
+
+	// Force redraw of the previously active tab.
+	_selectedIndex = 0;
+	self.selectedIndex = 0;
+}
+
+- (void)setViewControllerTitles:(NSArray *)viewControllerTitles
+{
+    _viewControllerTitles = viewControllerTitles;
+    [self reloadTabButtons];
+}
+
+- (void)layoutTabButtons
+{
+	NSUInteger index = 0;
+	NSUInteger count = [self.viewControllerTitles count];
+
+	CGRect rect = CGRectMake(0, 0, floorf(self.view.bounds.size.width / count), TAB_BAR_HEIGHT);
+
+	//indicatorImageView.hidden = YES;
+
+	NSArray *buttons = [tabButtonsContainerView subviews];
+	for (UIButton *button in buttons)
+	{
+		if (index == count - 1)
+			rect.size.width = self.view.bounds.size.width - rect.origin.x;
+
+		button.frame = rect;
+		rect.origin.x += rect.size.width;
+
+		if (index == self.selectedIndex)
+			[self centerIndicatorOnButton:button];
+
+		++index;
+	}
+}
+
+- (void)viewDidLoad
+{
+	[super viewDidLoad];
+
+    CGRect rect = CGRectMake(0, -0.5, self.view.bounds.size.width, TAB_BAR_HEIGHT);
+	tabButtonsContainerView = [[UIView alloc] initWithFrame:rect];
+	tabButtonsContainerView.autoresizingMask = UIViewAutoresizingFlexibleWidth;
+	[self.view addSubview:tabButtonsContainerView];
+
+    
+    UIImageView *topLine = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"words list_topLine.png"]];
+    topLine.alpha = 0.8f;
+    CGRect frame = topLine.frame;
+    frame.origin.y = TAB_BAR_HEIGHT - 3;
+    topLine.frame = frame;
+    
+    [self.view addSubview:topLine];
+    
+	indicatorImageView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"words list_triangle.png"]];
+	[self.view addSubview:indicatorImageView];
+
+    _selectedIndex = 0;
+    
+	[self reloadTabButtons];
+}
+
+- (void)viewDidUnload
+{
+	[super viewDidUnload];
+	tabButtonsContainerView = nil;
+	indicatorImageView = nil;
+}
+
+- (void)viewWillLayoutSubviews
+{
+	[super viewWillLayoutSubviews];
+	[self layoutTabButtons];
+}
+
+
+- (void)setViewControllers:(NSArray *)newViewControllers
+{
+	NSAssert([newViewControllers count] >= 2, @"MHTabBarController requires at least two view controllers");
+
+	if ([self isViewLoaded])
+		[self reloadTabButtons];
+}
+
+- (void)setSelectedIndex:(NSUInteger)newSelectedIndex
+{
+	[self setSelectedIndex:newSelectedIndex animated:NO];
+}
+
+- (void)setSelectedIndex:(NSUInteger)newSelectedIndex animated:(BOOL)animated
+{
+    if(newSelectedIndex == _selectedIndex)
+        return;
+    
+    if (_selectedIndex != NSNotFound)
+    {
+        UIButton *fromButton = (UIButton *)[tabButtonsContainerView viewWithTag:TAG_OFFSET + _selectedIndex];
+        [self deselectTabButton:fromButton];
+        NSLog(@"frome %@",fromButton);
+    }
+    
+    UIButton *toButton;
+    if (_selectedIndex != NSNotFound)
+    {
+        toButton = (UIButton *)[tabButtonsContainerView viewWithTag:TAG_OFFSET + newSelectedIndex];
+        [self selectTabButton:toButton];
+    }
+    _selectedIndex = newSelectedIndex;
+    NSLog(@"to %@",toButton);
+
+    if (animated)
+    {
+        tabButtonsContainerView.userInteractionEnabled = NO;
+        
+        [UIView animateWithDuration:0.3 animations:^()
+         {
+             [self centerIndicatorOnButton:toButton];
+         }
+                         completion:^(BOOL finished)
+         {
+             tabButtonsContainerView.userInteractionEnabled = YES;
+             
+         }];
+    }
+    else  // not animated
+    {
+        [self centerIndicatorOnButton:toButton];
+    }
+
+    [self.delegate mh_tabBarController:self didSelectIndex:newSelectedIndex];
+}
+
+- (void)tabButtonPressed:(UIButton *)sender
+{
+	[self setSelectedIndex:sender.tag - TAG_OFFSET animated:YES];
+}
+
+@end
