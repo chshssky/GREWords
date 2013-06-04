2e451579a988075cf107bcb23324c1ea7714a157
xsource <xsource@126.com>
2013-06-04 10:52:28 +0800

    progressBar finished

--- GreWords/GreWords/PresentationLayer/History/HistoryStatisticsViewController.m
+++ GreWords/GreWords/PresentationLayer/History/HistoryStatisticsViewController.m
@@ -13,6 +13,14 @@
 @interface HistoryStatisticsViewController ()
 @property (weak, nonatomic) IBOutlet UIImageView *greenCover;
 @property (weak, nonatomic) IBOutlet UIButton *progressButton;
+@property (weak, nonatomic) IBOutlet UIImageView *progressText;
+
+@property (nonatomic) CGPoint firstPoint;
+@property (nonatomic) CGPoint secondPoint;
+@property (nonatomic) CGPoint thridPoint;
+@property (nonatomic) CGPoint fourthPoint;
+@property (nonatomic) CGPoint lastPoint;
+@property (nonatomic) CGPoint choosePoint;
 
 @end
 
@@ -31,6 +39,9 @@
 {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
+    [self initialFourPoint];
+    [self initialLastPointWithPoint:_thridPoint];
+    [self setGesture];
 }
 
 - (void)didReceiveMemoryWarning
@@ -43,6 +54,7 @@
     [self setExitButton:nil];
     [self setGreenCover:nil];
     [self setProgressButton:nil];
+    [self setProgressText:nil];
     [super viewDidUnload];
 }
 - (IBAction)exitPressed:(id)sender {
@@ -67,4 +79,143 @@
     
     [self dismissModalViewControllerAnimated:YES];
 }
+
+- (void)initialFourPoint
+{
+    _firstPoint.x = 77.0f;
+    _firstPoint.y = 77.0f;
+    _secondPoint.x = 135.0f;
+    _secondPoint.y = 77.0f;
+    _thridPoint.x = 190.0f;
+    _thridPoint.y = 77.0f;
+    _fourthPoint.x = 245.0f;
+    _fourthPoint.y = 77.0f;
+}
+
+- (void)animationDidStop:(CAAnimation *)theAnimation finished:(BOOL)flag {
+    if([[theAnimation valueForKey:@"id"] isEqual:@"GoToPointFinished"])
+    {
+        if (_progressButton.center.x == _firstPoint.x) {
+            [_progressText setImage:[UIImage imageNamed:@"history_slideBar_text1.png"]];
+            _choosePoint = _firstPoint;
+        }else if (_progressButton.center.x == _secondPoint.x) {
+            [_progressText setImage:[UIImage imageNamed:@"history_slideBar_text2.png"]];
+            _choosePoint = _secondPoint;
+        }else if (_progressButton.center.x == _thridPoint.x) {
+            [_progressText setImage:[UIImage imageNamed:@"history_slideBar_text3.png"]];
+            _choosePoint = _thridPoint;
+        }else if (_progressButton.center.x == _fourthPoint.x) {
+            [_progressText setImage:[UIImage imageNamed:@"history_slideBar_text4.png"]];
+            _choosePoint = _fourthPoint;
+        }
+    }
+}
+
+- (void)initialLastPointWithPoint:(CGPoint)lastPoint
+{
+    _lastPoint = lastPoint;
+    _progressButton.center = lastPoint;
+    _greenCover.center = CGPointMake(_progressButton.center.x - 85.0f, _progressButton.center.y);
+    _choosePoint = lastPoint;
+    
+    if (lastPoint.x == _firstPoint.x) {
+        [_progressText setImage:[UIImage imageNamed:@"history_slideBar_text1.png"]];
+    }else if (lastPoint.x == _secondPoint.x) {
+        [_progressText setImage:[UIImage imageNamed:@"history_slideBar_text2.png"]];
+    }else if (lastPoint.x == _thridPoint.x) {
+        [_progressText setImage:[UIImage imageNamed:@"history_slideBar_text3.png"]];
+    }else if (lastPoint.x == _fourthPoint.x) {
+        [_progressText setImage:[UIImage imageNamed:@"history_slideBar_text4.png"]];
+    }
+}
+
+- (void)checkProgressButtonShouldMoveToWhichPoint
+{
+    if (_progressButton.center.x >= 77.0f && _progressButton.center.x <= 106.0f) {
+        [self goToPoint:_firstPoint];
+    }else if (_progressButton.center.x > 106.0f && _progressButton.center.x <= 163.0f) {
+        [self goToPoint:_secondPoint];
+    }else if (_progressButton.center.x > 163.0f && _progressButton.center.x <= 218.0f) {
+        [self goToPoint:_thridPoint];
+    }else if (_progressButton.center.x > 218.0f && _progressButton.center.x <= 245.0f) {
+        [self goToPoint:_fourthPoint];
+    }
+}
+
+- (void)goToPoint:(CGPoint)destinationPoint
+{
+    CAKeyframeAnimation *lineAnimation;
+    CGMutablePathRef path;
+    
+    lineAnimation = [CAKeyframeAnimation animationWithKeyPath:@"position"];
+    path = CGPathCreateMutable();
+    CGPathMoveToPoint(path, NULL, _progressButton.center.x, _progressButton.center.y);
+    CGPathAddLineToPoint(path, NULL, destinationPoint.x, destinationPoint.y);
+    lineAnimation.path = path;
+    CGPathRelease(path);
+    lineAnimation.duration = 0.15f;
+    lineAnimation.beginTime = CACurrentMediaTime();
+    lineAnimation.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseIn];
+    [_progressButton.layer addAnimation:lineAnimation forKey:nil];
+    
+    
+    lineAnimation = [CAKeyframeAnimation animationWithKeyPath:@"position"];
+    [lineAnimation setValue:@"GoToPointFinished" forKey:@"id"];
+    path = CGPathCreateMutable();
+    CGPathMoveToPoint(path, NULL, _progressButton.center.x - 85.0f, _progressButton.center.y);
+    CGPathAddLineToPoint(path, NULL, destinationPoint.x - 85.0f, destinationPoint.y);
+    lineAnimation.path = path;
+    CGPathRelease(path);
+    lineAnimation.duration = 0.15f;
+    lineAnimation.beginTime = CACurrentMediaTime();
+    [UIView setAnimationDidStopSelector:@selector(animationDidStop:finished:)];
+    lineAnimation.delegate = self;
+    lineAnimation.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseIn];
+    [_greenCover.layer addAnimation:lineAnimation forKey:nil];
+    
+    _progressButton.center = destinationPoint;
+    _greenCover.center = CGPointMake(_progressButton.center.x - 85.0f, _progressButton.center.y);
+}
+
+- (void)setGesture
+{
+    UILongPressGestureRecognizer *gestureRecognizer = [[UILongPressGestureRecognizer alloc] initWithTarget:self action:@selector(handle:)];
+    [_progressButton addGestureRecognizer:gestureRecognizer];
+    _progressButton.userInteractionEnabled = YES;
+    gestureRecognizer.minimumPressDuration = 0;
+}
+
+- (void)handle:(UILongPressGestureRecognizer *)recognizer
+{
+    
+    if (recognizer.state ==
+        UIGestureRecognizerStateBegan) {
+        [_progressButton setImage:[UIImage imageNamed:@"history progressBar_button_clicked.png"] forState:UIControlStateNormal];
+    }
+    
+    if (recognizer.state ==
+        UIGestureRecognizerStateChanged) {
+        CGPoint movePoint = [recognizer locationInView:self.view];
+        if (movePoint.x >= _firstPoint.x && movePoint.x <= _lastPoint.x) {
+            _progressButton.center = CGPointMake(movePoint.x, _progressButton.center.y);
+            _greenCover.center = CGPointMake(_progressButton.center.x - 85.0f, _progressButton.center.y);
+        }else if (movePoint.x < _firstPoint.x) {
+            _progressButton.center = _firstPoint;
+            _greenCover.center = CGPointMake(_progressButton.center.x - 85.0f, _progressButton.center.y);
+        }else if (movePoint.x > _lastPoint.x) {
+            _progressButton.center = _lastPoint;
+            _greenCover.center = CGPointMake(_progressButton.center.x - 85.0f, _progressButton.center.y);
+        }
+    }
+    
+    if (recognizer.state == UIGestureRecognizerStateEnded) {
+        [_progressButton setImage:[UIImage imageNamed:@"history progressBar_button.png"] forState:UIControlStateNormal];
+        [self checkProgressButtonShouldMoveToWhichPoint];
+    }
+}
+- (IBAction)progressButtonClicked:(id)sender {
+    NSLog(@"progressButtonClicked");
+}
+
+
 @end
