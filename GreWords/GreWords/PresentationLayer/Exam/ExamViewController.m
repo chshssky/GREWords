90ed42fbd8f44086e493586ba36f771d838e7f24
Song <sbhhbscbfv@gmail.com>
2013-06-09 23:42:03 +0800

    = =|||

--- GreWords/GreWords/PresentationLayer/Exam/ExamViewController.m
+++ GreWords/GreWords/PresentationLayer/Exam/ExamViewController.m
@@ -1357,27 +1357,6 @@
 {
     [self examResultShow];
     
-//    GreWordsViewController *superController =  (GreWordsViewController *)[self presentingViewController];
-//    
-//    UIImageView *blackView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height)];
-//    [blackView setBackgroundColor:[UIColor blackColor]];
-//    blackView.alpha = 0;
-//    [superController.view addSubview:blackView];
-//    
-//    
-//    superController.whetherAllowViewFrameChanged = YES;
-//    CABasicAnimation *opacityAnim_black = [CABasicAnimation animationWithKeyPath:@"opacity"];
-//    opacityAnim_black.fromValue = [NSNumber numberWithFloat:0.7];
-//    opacityAnim_black.toValue = [NSNumber numberWithFloat:0];
-//    opacityAnim_black.removedOnCompletion = YES;
-//    CAAnimationGroup *animGroup_black = [CAAnimationGroup animation];
-//    animGroup_black.animations = [NSArray arrayWithObjects:opacityAnim_black, nil];
-//    animGroup_black.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut];
-//    animGroup_black.duration = 0.5;
-//    [blackView.layer addAnimation:animGroup_black forKey:nil];
-//    
-//    [self dismissModalViewControllerAnimated:YES];
-    
 }
 
 #pragma mark -  exam result delegate
@@ -1389,6 +1368,26 @@
 -(void)backLHome
 {
     NSLog(@"backLHome");
+    GreWordsViewController *superController =  (GreWordsViewController *)[self presentingViewController];
+    
+    UIImageView *blackView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height)];
+    [blackView setBackgroundColor:[UIColor blackColor]];
+    blackView.alpha = 0;
+    [superController.view addSubview:blackView];
+    
+    
+    superController.whetherAllowViewFrameChanged = YES;
+    CABasicAnimation *opacityAnim_black = [CABasicAnimation animationWithKeyPath:@"opacity"];
+    opacityAnim_black.fromValue = [NSNumber numberWithFloat:0.7];
+    opacityAnim_black.toValue = [NSNumber numberWithFloat:0];
+    opacityAnim_black.removedOnCompletion = YES;
+    CAAnimationGroup *animGroup_black = [CAAnimationGroup animation];
+    animGroup_black.animations = [NSArray arrayWithObjects:opacityAnim_black, nil];
+    animGroup_black.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut];
+    animGroup_black.duration = 0.5;
+    [blackView.layer addAnimation:animGroup_black forKey:nil];
+    
+    [self dismissModalViewControllerAnimated:YES];
 }
 
 @end
