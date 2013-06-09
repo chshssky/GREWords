d5c5c3a1660324ef0de39bf4793969b123eb5ae6
chshssky <chshssky@gmail.com>
2013-06-10 04:20:28 +0800

    测试时，单词太少，已提示

--- GreWords/GreWords/PresentationLayer/Exam/ExamViewController.m
+++ GreWords/GreWords/PresentationLayer/Exam/ExamViewController.m
@@ -229,34 +229,34 @@
 - (void)viewDidAppear:(BOOL)animated
 {
     [self.timer fire];
-    if (self.examArr == nil) {
-        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"无法测试" message:[NSString stringWithFormat:@"由于您的智能词表里的词数少于30，无法测试"] delegate:self cancelButtonTitle:@"知道了" otherButtonTitles:nil];
-        [alert setAlertViewStyle:UIAlertViewStyleDefault];
-        
-        [alert show];
-    
-
-        GreWordsViewController *superController =  (GreWordsViewController *)[self presentingViewController];
-        
-        UIImageView *blackView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height)];
-        [blackView setBackgroundColor:[UIColor blackColor]];
-        blackView.alpha = 0;
-        [superController.view addSubview:blackView];
-        
-        
-        superController.whetherAllowViewFrameChanged = YES;
-        CABasicAnimation *opacityAnim_black = [CABasicAnimation animationWithKeyPath:@"opacity"];
-        opacityAnim_black.fromValue = [NSNumber numberWithFloat:0.7];
-        opacityAnim_black.toValue = [NSNumber numberWithFloat:0];
-        opacityAnim_black.removedOnCompletion = YES;
-        CAAnimationGroup *animGroup_black = [CAAnimationGroup animation];
-        animGroup_black.animations = [NSArray arrayWithObjects:opacityAnim_black, nil];
-        animGroup_black.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut];
-        animGroup_black.duration = 0.5;
-        [blackView.layer addAnimation:animGroup_black forKey:nil];
-        
-        [self dismissModalViewControllerAnimated:YES];
-    }
+//    if (self.examArr == nil) {
+//        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"无法测试" message:[NSString stringWithFormat:@"由于您的智能词表里的词数少于30，无法测试"] delegate:self cancelButtonTitle:@"知道了" otherButtonTitles:nil];
+//        [alert setAlertViewStyle:UIAlertViewStyleDefault];
+//        
+//        [alert show];
+//    
+//
+//        GreWordsViewController *superController =  (GreWordsViewController *)[self presentingViewController];
+//        
+//        UIImageView *blackView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height)];
+//        [blackView setBackgroundColor:[UIColor blackColor]];
+//        blackView.alpha = 0;
+//        [superController.view addSubview:blackView];
+//        
+//        
+//        superController.whetherAllowViewFrameChanged = YES;
+//        CABasicAnimation *opacityAnim_black = [CABasicAnimation animationWithKeyPath:@"opacity"];
+//        opacityAnim_black.fromValue = [NSNumber numberWithFloat:0.7];
+//        opacityAnim_black.toValue = [NSNumber numberWithFloat:0];
+//        opacityAnim_black.removedOnCompletion = YES;
+//        CAAnimationGroup *animGroup_black = [CAAnimationGroup animation];
+//        animGroup_black.animations = [NSArray arrayWithObjects:opacityAnim_black, nil];
+//        animGroup_black.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut];
+//        animGroup_black.duration = 0.5;
+//        [blackView.layer addAnimation:animGroup_black forKey:nil];
+//        
+//        [self dismissModalViewControllerAnimated:YES];
+//    }
 }
 
 - (void)ShowMeaning
